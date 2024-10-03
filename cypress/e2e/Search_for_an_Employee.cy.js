36d846b70ef2812eb286a288296a02ca03716893
immobarok <immobarokali2001@gmail.com>
2024-10-03 15:08:42 +0600

    Search employee

--- cypress/e2e/Search_for_an_Employee.cy.js
+++ cypress/e2e/Search_for_an_Employee.cy.js
@@ -3,24 +3,14 @@ describe('OrangeHRM Employee Search', () => {
 
 
   beforeEach(() => {
-
+    login('m0bar0k', 'M0barok1234@');
   });
 
-  it('Should navigate to the Admin section successfully', () => {
-    cy.get(':nth-child(1) > .oxd-main-menu-item').click();
-    cy.url().should('include', '/admin/viewSystemUsers');
-  });
-
-  it('Should successfully add a new employee', () => {
-    cy.get(':nth-child(2) > .oxd-main-menu-item').click();
-    cy.get('.oxd-button').contains('Add').click();
-
-    cy.get('input[name="firstName"]').type('Ashraful');
-    cy.get('input[name="lastName"]').type('Islam');
+  it('Should search for an employee by name', () => {
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click(); // Click on PIM section
+    cy.get('input[placeholder="Employee Name"]').type('Ashraful Islam'); // Search employee
     cy.get('button[type="submit"]').click();
-
-    cy.url().should('include', '/pim/viewPersonalDetails');
-    cy.get('.orangehrm-edit-employee-name').should('contain', 'Ashraful');
+    cy.get('.oxd-table-body').should('contain', 'Ashraful Islam'); // Verify result
   });
 
 });
