1b0f7117ac44b9b60e092407ab74e5757463307b
immobarok <immobarokali2001@gmail.com>
2024-10-10 21:38:09 +0600

    DeleteEmployeeFromEmployeeListIfExists

--- cypress/e2e/Delete_a_User_from_employeeList.cy.js
+++ cypress/e2e/Delete_a_User_from_employeeList.cy.js
@@ -1,20 +1,22 @@
 import { login } from './Login.cy';
 
-describe('Delete if exists an user from empolyee list', () => {
+describe('Delete if exists an user from employee list', () => {
   beforeEach(() => {
     login('m0bar0k', 'M0barok1234@'); // Use actual credentials
   });
 
-  it('Should successfully Delete', () => {
+  it('Should successfully delete an employee', () => {
+    // Navigate to the employee list
     cy.get(':nth-child(2) > .oxd-main-menu-item').click();
 
     cy.url().should('include', 'pim/viewEmployeeList');
 
     cy.contains('Employee List').click();
 
-    cy.get('oxd-icon-button').eq(1).click();
-
-
+    // Look for the delete button by its class and click it
+    cy.get('.oxd-icon.bi-trash').first().click();
+    // Add assertion to ensure you are still on the employee list page
+    cy.contains(" Yes, Delete ").click();
     cy.url().should('include', '/viewEmployeeList');
   });
 });
